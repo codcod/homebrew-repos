8ff2d9a8648700c2d081bdd21c2ffbc409f21bbc
codcod <nikos@linux.com>
2025-05-18 11:19:44 +0200

    Fixes

--- repos.rb
+++ repos.rb
@@ -1,17 +1,17 @@
 class Repos < Formula
   desc "CLI tool to operate multiple GitHub repos"
   homepage "https://github.com/codcod/repos"
-  url "https://github.com/codcod/repos/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "894a31867e905d54ec3327f83ee55f202fe7b6e547eebbfeda926eb306db54b5"
+  url "https://github.com/codcod/repos/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "cb3e3d2ec53b3d714e46d2a48562ac82bb8f8fbe8b6f49bfce910dd447106151"
   license "MIT" # or whatever license you use
 
   depends_on "go" => :build
 
   def install
-    system "go", "build ./cmd/repos", *std_go_args
+    system "go", "build", "-o", bin/"repos", "./cmd/repos"
   end
 
   test do
-    system "bin/"repos", "--version"
+    system "bin/repos", "--version"
   end
 end
